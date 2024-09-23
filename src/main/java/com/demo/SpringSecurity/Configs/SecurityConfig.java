60cc7b443801c8760f99bce28f486893da91b34c
CRISTIANRENDON17 <cristianrendon1704@gmail.com>
2024-09-23 13:10:08 -0500

    Tercer Commit, se crean configuraciones de autenticacion

--- src/main/java/com/demo/SpringSecurity/Configs/SecurityConfig.java
+++ src/main/java/com/demo/SpringSecurity/Configs/SecurityConfig.java
@@ -4,16 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // configuration one -> 
+   
+    /**
+     * Configura la cadena de filtros de seguridad.
+     * 
+     * @param http Objeto HttpSecurity para configurar la seguridad de las solicitudes HTTP.
+     * @return SecurityFilterChain configurada.
+     * @throws Exception si ocurre algún error durante la configuración.
+     */
+    /*  
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> csrf.disable()) // Desactiva la protección CSRF.
+            .csrf(csrf -> csrf.disable()) // Desactiva la protección CSRF (Cross-Site Request Forgery).
+                // Las aplicaciones que exponen solo servicios REST suelen ser stateless (sin estado) y usan tokens como JWT (JSON Web Tokens) para la autenticación.
+                // Dado que no tienen un estado de sesión del lado del servidor, la protección CSRF no es relevante o necesaria para proteger las solicitudes.
+                //Es común desactivar CSRF en aplicaciones que ofrecen APIs RESTful para simplificar la seguridad y evitar errores innecesarios.
+            
             .authorizeHttpRequests(authz -> authz // Inicia la configuración de autorizaciones para las solicitudes HTTP.
                 .requestMatchers("/v1/index2").permitAll() // Permite acceso sin autenticación al endpoint /v1/index2.
                 .anyRequest().authenticated() // Requiere autenticación para cualquier otra solicitud.
@@ -21,6 +39,89 @@ public class SecurityConfig {
             .formLogin(form -> form.permitAll()) // Permite el acceso al formulario de inicio de sesión.
             .build(); // Retorna el objeto SecurityFilterChain configurado.
     } 
+ */
 
+// --------------------------------------------------------------------------------------------
+
+    // configuration two ->     
+     /**
+     * Configura la cadena de filtros de seguridad.
+     * 
+     * @param http Objeto HttpSecurity para configurar la seguridad de las solicitudes HTTP.
+     * @return SecurityFilterChain configurada.
+     * @throws Exception si ocurre algún error durante la configuración.
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable()) // Desactiva la protección CSRF (Cross-Site Request Forgery).
+                /*
+                 * Las aplicaciones que exponen solo servicios REST suelen ser stateless (sin estado) y usan tokens como JWT (JSON Web Tokens) para la autenticación.
+                 * Dado que no tienen un estado de sesión del lado del servidor, la protección CSRF no es relevante o necesaria para proteger las solicitudes.
+                 * Es común desactivar CSRF en aplicaciones que ofrecen APIs RESTful para simplificar la seguridad y evitar errores innecesarios.
+                 */
+            .authorizeHttpRequests(authz -> authz // Inicia la configuración de autorizaciones para las solicitudes HTTP.
+                .requestMatchers("/v1/index2").permitAll() // Permite acceso sin autenticación al endpoint /v1/index2.
+                .anyRequest().authenticated() // Requiere autenticación para cualquier otra solicitud.
+            )
+            .formLogin(form -> form // Habilita la autenticación basada en formulario.
+                .successHandler(successHandler()) // Especifica el manejador de éxito al autenticarse correctamente.
+                .permitAll() // Permite el acceso al formulario de inicio de sesión a cualquier usuario.
+            )
+            .sessionManagement(session -> session // Configura la administración de sesiones.
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) 
+                /* 
+                 * Configura cómo Spring Security gestionará la creación de sesiones.
+                 * - ALWAYS: Siempre creará una nueva sesión si no existe y reutilizará si ya existe.
+                 * - IF_REQUIRED: Crea una nueva sesión solo si es necesario y reutiliza si ya existe.
+                 * - NEVER: No crea sesiones, pero reutiliza si ya hay una sesión existente.
+                 * - STATELESS: No crea ni reutiliza sesiones; útil para servicios sin estado como RESTful APIs.
+                 */
+                .invalidSessionUrl("/login") // URL de redirección cuando la sesión es inválida.
+                .maximumSessions(1) // Limita a 1 la cantidad de sesiones activas por usuario.
+                /* 
+                 * Útil para aplicaciones que desean evitar que un usuario tenga múltiples sesiones activas.
+                 * Para aplicaciones multiplataforma, se podría aumentar este número.
+                 */
+                .expiredUrl("/login") // URL de redirección cuando la sesión expira por inactividad.
+                .sessionRegistry(sessionRegistry()) // podemos definir o inyectar un objeto que administre la sesion
+            )
+            .sessionManagement(session -> session
+                .sessionFixation().migrateSession()) 
+            /* 
+             * Configura la migración de la sesión para protegerse contra ataques de fijación de sesión.
+             * migrateSession(): Crea una nueva sesión al autenticarse y transfiere los atributos de la sesión original.
+             * newSession():  Crea una nueva sesión al autenticarse y NO transfiere los atributos de la sesión original, NI SUS DATOS.
+             * none(): Inhabilitar la seguridad, permite al atacante apropiarse de la session
+             */
+            .build(); // Retorna el objeto SecurityFilterChain configurado.
+    }
+
+    /**
+     * Define un manejador de éxito personalizado para redirigir a un endpoint específico después de la autenticación exitosa.
+     * 
+     * @return AuthenticationSuccessHandler configurado.
+     */
+    public AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            response.sendRedirect("/v1/index"); // Redirige al endpoint /v1/index tras autenticación exitosa.
+        };
+    }
+
+    /**
+     * Define un bean de tipo SessionRegistry que gestiona las sesiones de los usuarios autenticados.
+     * 
+     * @return una instancia de SessionRegistryImpl que se utilizará para registrar, manejar y consultar 
+     *         las sesiones activas de los usuarios en la aplicación.
+     * 
+     * La clase SessionRegistryImpl es la implementación por defecto de la interfaz SessionRegistry
+     * que se encarga de llevar un seguimiento de todas las sesiones de los usuarios en la aplicación.
+     * Esto es útil, por ejemplo, para controlar el número de sesiones activas de un usuario o para
+     * obtener información de los usuarios que han iniciado sesión.
+     */
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
 }
--- src/main/java/com/demo/SpringSecurity/Controllers/CustomerController.java
+++ src/main/java/com/demo/SpringSecurity/Controllers/CustomerController.java
@@ -1,6 +1,14 @@
 package com.demo.SpringSecurity.Controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class CustomerController {
 
+    @Autowired
+    private SessionRegistry sessionRegistry;
+
     @GetMapping("/index")
     public String index() {
         return "Hello word!";
@@ -16,17 +27,31 @@ public class CustomerController {
 
     @GetMapping("/index2")
     public String index2() {
-        return "Hello word not SECURED";
-    }
-    @GetMapping("/index3")
-    public ResponseEntity<?> index3() {
-        String message = "Hello world!"; // Corregido el mensaje
-        return ResponseEntity.ok(message);
+        return "Hello word not SECURED!!";
     }
 
-    @GetMapping("/index4")
-    public ResponseEntity<?> index4() {
-        String message = "Hello world not SECURED"; // Corregido el mensaje
-        return ResponseEntity.ok(message);
+    @GetMapping("/session")
+    public ResponseEntity<?> getDetailSessions() {
+        String sessionId = "";
+        User userObject = null; // objeto definido dentro de spring security, representa un usuario
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals(); // nos devuelve una lista de objetos
+
+        for(Object session: sessions){
+            if(session instanceof User){
+                userObject = (User) session;
+            }
+            List<SessionInformation> sessionInformations =  sessionRegistry.getAllSessions(session, false); 
+            // usuario que estamos recupernado session, y boolean en falso (incluir sessions invalidas)
+            for(SessionInformation sessionInformation: sessionInformations){
+                sessionId = sessionInformation.getSessionId(); // recuperamos el id de la session
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("response", "Hello word!");
+        response.put("sessionId", sessionId);
+        response.put("sessionUser", userObject);
+        return ResponseEntity.ok(response);
     }
 }
